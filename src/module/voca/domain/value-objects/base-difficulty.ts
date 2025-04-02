79b8ad8809fc1c532e5a3c7482680bb639df5c4f
이정원 <wjddnjs7895@naver.com>
2025-04-02 19:41:01 +0900

    refactor: relocate files

--- src/module/voca/domain/value-objects/base-difficulty.ts
+++
@@ -1,3 +0,0 @@
-export abstract class BaseDifficulty {
-  abstract getLevel(): number;
-}
--- src/module/voca/presentation/voca.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Controller } from '@nestjs/common';
-import { VocaFacade } from '../application/voca.facade';
-@Controller('voca')
-export class VocaController {
-  constructor(private readonly vocaFacade: VocaFacade) {}
-}
